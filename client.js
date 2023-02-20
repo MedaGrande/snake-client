5da84dc35ca71255b43eb186f82798f3f6a87f5b
Medhanie Alene <alenemedhanie@gmail.com>
2023-02-19 20:57:07 -0500

    named our snake by sending it to the server

--- client.js
+++ client.js
@@ -6,13 +6,17 @@ const connect = function () {
     host: 'localhost', // IP address here,
     port: 50541  // PORT number here,
   });
-  
+  //message after successful connection
   conn.on("connect", (data) => {
     console.log("Successfully connected to game server!");
   });
-  
+
   conn.on("connect", () => {
     conn.write("Hello from client!");
+  });
+  //sending our initials to the server
+  conn.on("connect", () => {
+    conn.write("Name: MGA");
   });
 
   // interpret incoming data as text
