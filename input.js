98f1915dd3009b8375451f225aba9b226d0dbdc2
Medhanie Alene <alenemedhanie@gmail.com>
2023-02-21 19:01:34 -0500

    added WASD keys assignment to move the snake around

--- input.js
+++ input.js
@@ -1,17 +1,28 @@
 // setup interface to handle user input from stdin
-
-const setupInput = function () {
+let connection;
+const setupInput = function (conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
+  connection = conn;
   return stdin;
 };
 
+//assign keys to send to the server
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
+  }
+  if (key === 'w') {
+    connection.write("Move: up");
+  } else if (key === 'a') {
+    connection.write("Move: left");
+  } else if (key === 's') {
+    connection.write("Move: down");
+  } else if (key === 'd') {
+    connection.write("Move: right");
   }
 };
 
