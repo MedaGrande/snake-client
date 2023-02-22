1e744db5306ea1012ee928fc784677241be02c26
Medhanie Alene <alenemedhanie@gmail.com>
2023-02-22 16:25:21 -0500

    file linted

--- constants.js
+++ constants.js
@@ -1,9 +1,9 @@
 const IP = 'localhost';
 const PORT = 50541;
-const moveUp = "Move: up"
-const moveDown = "Move: down"
-const moveRight = "Move: right"
-const moveLeft = "Move: left"
+const moveUp = "Move: up";
+const moveDown = "Move: down";
+const moveRight = "Move: right";
+const moveLeft = "Move: left";
 
 
 module.exports = {
