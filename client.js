d1ba4fea27cc2574e4b215f522a5bbb4ced0ee1a
Medhanie Alene <alenemedhanie@gmail.com>
2023-02-22 16:25:41 -0500

    file linted and cleaned

--- client.js
+++ client.js
@@ -2,21 +2,20 @@ const net = require("net");
 const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
-    host: IP, // IP address here,
-    port: PORT  // PORT number here,
+    host: IP, // IP address from constants,
+    port: PORT  // PORT number from constants,
   });
   //message after successful connection
-  conn.on("connect", (data) => {
+  conn.on("connect", () => {
     console.log("Successfully connected to game server!");
   });
-
+  //sending message to the server
   conn.on("connect", () => {
     conn.write("Hello from client!");
   });
   //sending our initials to the server
-  //sending move messages to the server
   conn.on("connect", () => {
     conn.write("Name: MGA");
   });
@@ -26,7 +25,6 @@ const connect = function () {
   conn.on("data", (data) => {
     console.log("Server says: ", data);
   });
-
   return conn;
 };
 
