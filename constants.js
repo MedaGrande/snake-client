cc508c734e31398539c56421d9399c19b219fe04
Medhanie Alene <alenemedhanie@gmail.com>
2023-02-21 23:33:08 -0500

    moved WASD to constants

--- constants.js
+++ constants.js
@@ -1,7 +1,16 @@
 const IP = 'localhost';
 const PORT = 50541;
+const moveUp = "Move: up"
+const moveDown = "Move: down"
+const moveRight = "Move: right"
+const moveLeft = "Move: left"
+
 
 module.exports = {
   IP,
   PORT,
+  moveUp,
+  moveDown,
+  moveRight,
+  moveLeft
 };
